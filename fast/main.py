ba56c709219d3dc45903c5beed16bef9246179bc
ae <amine.bachri@epitech.eu>
2022-08-22 18:36:45 +0200

    svelte.

--- fast/main.py
+++ fast/main.py
@@ -4,14 +4,13 @@ from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
+app.mount("/front", StaticFiles(directory="front/public", html=True), name="front")
+app.mount("/build", StaticFiles(directory="front/public/build"), name="build")
+
 
 @app.get("/toto")
 async def root():
     return {"message": "ok"}
-
-
-app.mount("/front", StaticFiles(directory="front/public", html=True), name="front")
-app.mount("/build", StaticFiles(directory="front/public/build"), name="build")
 
 
 @app.get("/")

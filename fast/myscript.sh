e4bddcd2b91d5b766f7243ea500246e2466cb991
ae <amine.bachri@epitech.eu>
2022-08-23 18:05:34 +0200

    ok

--- fast/myscript.sh
+++ fast/myscript.sh
@@ -5,10 +5,3 @@ uvicorn main:app --port 8100 --host '::' --proxy-headers --forwarded-allow-ips "
 cd front
 npm install
 npm run build
-
-<<<<<<< HEAD
-
-#end
-=======
-##
->>>>>>> 36e0aff2a1833dd68b7935b7a8ca6eb90a7ab72b

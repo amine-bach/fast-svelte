faf4848bf71be9f6ece097e44d9a15027c193760
ae <amine.bachri@epitech.eu>
2022-08-24 09:22:02 +0200

    consitional svelte

--- fast/main.py
+++ fast/main.py
@@ -5,17 +5,24 @@ from starlette.responses import RedirectResponse
 app = FastAPI()
 
 # https://css-tricks.com/what-i-like-about-writing-styles-with-svelte/
-app.mount("/svelte", StaticFiles(directory="svelte/public", html=True), name="svelte")
-app.mount(
-    "/built-svelte", StaticFiles(directory="svelte/public/build"), name="built-svelte"
-)
+
+is_svelte = False
+
+if is_svelte:
+    app.mount(
+        "/svelte", StaticFiles(directory="svelte/public", html=True), name="svelte"
+    )
+    app.mount(
+        "/built-svelte",
+        StaticFiles(directory="svelte/public/build"),
+        name="built-svelte",
+    )
+
+    @app.get("/")
+    async def svelte():
+        return RedirectResponse(url="svelte")
 
 
 @app.get("/toto")
 async def root():
     return {"message": "ok"}
-
-
-# @app.get("/")
-# async def svelte():
-#     return RedirectResponse(url="svelte")

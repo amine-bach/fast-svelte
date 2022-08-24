2f7ed82475263de43fc69c47b23b7a74d3972eba
ae <amine.bachri@epitech.eu>
2022-08-24 09:19:21 +0200

    test ko a priori

--- fast/main.py
+++ fast/main.py
@@ -4,11 +4,11 @@ from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
-# # https://css-tricks.com/what-i-like-about-writing-styles-with-svelte/
-# app.mount("/svelte", StaticFiles(directory="svelte/public", html=True), name="svelte")
-# app.mount(
-#     "/built-svelte", StaticFiles(directory="svelte/public/build"), name="built-svelte"
-# )
+# https://css-tricks.com/what-i-like-about-writing-styles-with-svelte/
+app.mount("/svelte", StaticFiles(directory="svelte/public", html=True), name="svelte")
+app.mount(
+    "/built-svelte", StaticFiles(directory="svelte/public/build"), name="built-svelte"
+)
 
 
 @app.get("/toto")
@@ -16,6 +16,6 @@ async def root():
     return {"message": "ok"}
 
 
-# @app.get("/")
-# async def svelte():
-#     return RedirectResponse(url="svelte")
+@app.get("/")
+async def svelte():
+    return RedirectResponse(url="svelte")
